54ab4a7e51fc5a82b1529653344608bccf49cb0d
Aleandro Ceruti <alceblockdev@gmail.com>
2026-03-05 17:06:48 +0000

    First test pipeline

---
+++ CI_CD/automated_pipeline.py
@@ -0,0 +1,12 @@
+# Sviluppare una pipeline automatizzata per il training del modello, i test di integrazione e il deploy dell'applicazione su HuggingFace.
+def automated_training():
+    # call training function
+    print("automated training ongoing....")
+
+def automated_integration_test():
+    # call training function
+    print("automated integration test ongoing....")
+
+def automated_deploy():
+    # call training function
+    print("automated deploy to hugging face ongoing....")
