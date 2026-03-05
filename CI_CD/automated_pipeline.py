94db8da4742bec77b90dcd5938b839b8a8c8ed6f
Aleandro Ceruti <alceblockdev@gmail.com>
2026-03-05 17:40:33 +0000

    test test_root_endpoint into pipeline

--- CI_CD/automated_pipeline.py
+++ CI_CD/automated_pipeline.py
@@ -1,4 +1,8 @@
 # Sviluppare una pipeline automatizzata per il training del modello, i test di integrazione e il deploy dell'applicazione su HuggingFace.
+
+from tests.test_model_inference import test_root_endpoint
+
+
 def test_automated_training():
     # call training function
     print("automated training ongoing....")
@@ -7,6 +11,8 @@ def test_automated_training():
 def test_automated_integration_test():
     # call training function
     print("automated integration test ongoing....")
+    test_root_endpoint()
+    print("automated integration test COMPLETED")
 
 def test_automated_deploy():
     # call training function
