80568acf432f4a8bc1a188c0f9b6cd7770c79653
Aleandro Ceruti <alceblockdev@gmail.com>
2026-03-07 21:50:09 +0000

    test monitoring

--- model/model_inference.py
+++ model/model_inference.py
@@ -18,11 +18,36 @@ sentiment_task = pipeline(
 class TextInput(BaseModel):
     text: str
 
+#####
+from prometheus_fastapi_instrumentator import Instrumentator
+import time
+
+# Inizializzazione prometheus instrumentator
+instrumentator = Instrumentator()
+instrumentator.instrument(app).expose(app)
+#####
+
 @app.post("/predict")
 async def predict(data: TextInput):
+    #####
+    start_time = time.time()
+    #####
     result = sentiment_task(data.text)[0]
+
+    #####
+    latency = time.time() - start_time
+
+    # Custom metrics, es. latency
+    from prometheus_client import Summary
+    REQUEST_LATENCY = Summary('model_inference_latency_seconds', 'Latency per inference')
+    REQUEST_LATENCY.observe(latency)
+    #####
 
     return {
         "label": result["label"],
         "confidence": result["score"]
     }
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
